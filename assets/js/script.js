a0fe299e8fccfda946a6d33f3932437ce0dd9478
stewart gibbons <thor@bluestrike.co.uk>
2024-03-22 18:28:01 +0000

    connect up files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 
+console.log ("connected")
 
 
-
+//* Block comment  
+/*
+*/
 function RandomNo () {
-    Math.floor(Math.random() * 125) + 1;
+    let num1 = Math.floor(Math.random() * 125) + 1;
 }
+
+
+console.log 'num1';
