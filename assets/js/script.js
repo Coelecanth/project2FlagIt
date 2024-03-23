29b5bd7983098957ca2f4568eb50343a93dccba9
stewart gibbons <thor@bluestrike.co.uk>
2024-03-23 15:29:51 +0000

    added functions for game execution

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,51 @@
 console.log ("connected")
 
 
-//* Block comment  
-/*
+
+/** Block comment  
+* randon Number function 
 */
 function RandomNo () {
     let num1 = Math.floor(Math.random() * 125) + 1;
+    console.log (num1)
 }
 
+/**
+ * comparison for Index returned 
+ */
+// function compareInd  () {
+//     let IsCorrect = ReturnedIndx === ActualIndx
+// }
 
+/**
+ * example function for checking answer 
+ */
+function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementIncorrectScore();
+    }
+
+    /**
+     * function to increment the correct answer score  
+     */
+    incrementScore () { 
+        let oldScore = parseInt (document.getElementById("correct").innerHTML);
+        document.getElementById("correct").innerText = ++oldScore; 
+    }
+
+     /**
+     * function to increment the corect answer score  
+     */
+    incrementIncorrectScore () { 
+        let oldScore = parseInt (document.getElementById("incorrect").innerHTML);
+        document.getElementById("incorrect").innerText = ++oldScore; 
+    }
