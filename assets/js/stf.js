bb0daeb118337dc9d55109e2bc8f4a7f34358b38
stewart gibbons <thor@bluestrike.co.uk>
2024-04-15 10:05:27 +0000

    added js files and added code

---
+++ assets/js/stf.js
@@ -0,0 +1,20 @@
+
+
+console.log("connected")
+const btnStudy = document.getElementById("study-btn");
+
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+generateStudy();
+
+    function generateStudy() {
+        // generate a dynamic list of all countries on page load
+        countryArray.forEach(country => {
+            studyContainer.innerHTML += `
+                <div class="study-row txt-white modal-open" data-modal="study" 
+                data-country="${country.locale}" 
+                data-iso="${country.flag-name}">
+                </div>`;
+        });
+    }
