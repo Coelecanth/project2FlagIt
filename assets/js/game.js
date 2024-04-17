150cb736955ddfae06cdd647e5566dbb3f5978af
stewart gibbons <thor@bluestrike.co.uk>
2024-04-17 18:33:05 +0000

    added js code to game script

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,73 +4,39 @@ const totalQuestions = document.getElementById("questions-total");
 const askedQuestions = document.getElementById("questions-asked");
 const correctQuestions = document.getElementById("questions-correct");
 const wrongQuestions = document.getElementById("questions-wrong");
-const gameStart = document.getElementsByTagName("button");
+const gameStart = document.getElementById("start-game");
 
-// comment - load flag index 
-// call random to get random index flag 
-// 
-//display flag -eg use index to reference img asset
-//user now selcts radio button 
-//listen for submit
+// set max question and write to page 
+totalQuestions.innerText = maxQuestions;
 
 console.log("connected")
 
-totalQuestions.innerText = maxQuestions;
-
 // Wait for the DOM to finish loading before running the game startr
-// Get the button elements and add event listeners for 
-
-// gameStart.addEventListener("click", initNewgame);
-
+// Get the button for start game and add event listeners for click.
 document.addEventListener("DOMContentLoaded", function() {
-        for (let button of gameStart ) {
-        button.addEventListener("click", function() {
-            switch (selectedGame) {
-                case "allflags-game":
-                    maxQuestions = countryArray.length;
-                    totalQuestions.innerText = maxQuestions;
-                    newAllFlags()
-                    break;
-        
-                case "europe-game":
-                    maxQuestions = 25;
-                    newAllFlags()
-                    break;
-                case "americas-game":
-                    maxQuestions = 25;
-                    break;
-                     
-                case "asia-game":
-                    maxQuestions = 25;
-                    break;
-                case "oceania-game":
-                    maxQuestions = 25;
-                    break;
-                case "africa - game":
-                    maxQuestions = 25;
-                    break;
-                default:
-                    selectedGame = "short-game"
-                    maxQuestions = 25;
-                    break;
-            }
-        });
-    }
-});
-
+         gameStart.addEventListener("click", initNewgame);
+       
+    });
 function initNewgame() {
     alert("submit pressed");
-    // determine which game has been selected
-   
-
-
-
 }
+    //load countries - eg 1 x flag and 4 x country names  
+    // write flag to Page 
+    // write 4 x country names to asnwer 1 ,2,3,4 
+    // listen for buttons answer - 
+    
+    // retrieve country name e.g read inner html)
+    
+    // compare answer to country name correct or Incorect 
+    // ?? right to innerghtml green or red, incrrment scores  
+    //   
+
+
 
 // call function to shuffle the selectedCountries
-shuffleCountries(selectedCountries);
+// shuffleCountries(selectedCountries);
 
-function shuffleCountries(selectedCountries) {
+// function shuffleCountries(selectedCountries) {
     // randomly shuffle the array of selectedCountries
-    return selectedCountries.sort(() => Math.random() - 0.5);
-}
+    //return selectedCountries.sort(() => Math.random() - 0.5);
+//}
