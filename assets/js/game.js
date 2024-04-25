28a83b62aa5d54beb6005169e52268e72fc3ae8a
stewart gibbons <thor@bluestrike.co.uk>
2024-04-25 18:17:58 +0000

    Bug/feature-fixed bug, add end of game feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@
 let shuffled;
 let sumQuestions;
 var idButton;
-let maxQuestions = 25;
+let maxQuestions = 4;
 let intialCorrect = 0;
 let intialWrong = 0;
 let intialAsked = 0;
@@ -16,27 +16,37 @@ const askedQuestions = document.getElementById("questions-asked");
 const correctQuestions = document.getElementById("questions-correct");
 const wrongQuestions = document.getElementById("questions-wrong");
 const gameStart = document.getElementById("start-game");
+const gameImg = "assets/img/start-game-new.avif"
 
+console.log("connected")
 
 resetScore()
 // set max question and zero values and write to page 
-function resetScore () {
-totalQuestions.innerText = maxQuestions;
-correctQuestions.innerText = intialCorrect
-wrongQuestions.innerText = intialWrong
-askedQuestions.innerText = intialAsked
+function resetScore() {
+    totalQuestions.innerText = maxQuestions;
+    correctQuestions.innerText = intialCorrect
+    wrongQuestions.innerText = intialWrong
+    askedQuestions.innerText = intialAsked
 }
 
-console.log("connected")
 
 // Wait for the DOM to finish loading before running the game start
 // Get the button for start game and add event listeners for click.
 $(document).ready(function () {
+    $("#game-over-mess").hide();
+    $("#next-button").hide();
+    $("#reset-game").hide();
     $("#start-game").click(initNewgame);
-        
+
 });
 
+
+
 function initNewgame() {
+    // show next and reset tbutton after start button pressed 
+    $("#game-over-mess").hide();
+    $("#next-button").show();
+    $("#reset-game").show();
     // Shuffle array, loads the array and shuffles it  
     shuffled = countryArray.sort(() => 0.5 - Math.random());
     // disable game options button 
@@ -45,7 +55,7 @@ function initNewgame() {
 }
 
 function getFlag() {
-    
+
     $(flagImage).attr("src", `assets/img/${shuffled[currentFlag].flagName}`);
     $(flagChoices).each(function (i, choice) {
         let buttonId = choice.id;
@@ -64,7 +74,7 @@ $("#reset-game").on("click", function () {
 
 // listen for click of all answer buttons
 $(".flag-choice").on("click", function () {
-    
+
     // capture ID of clicked button 
     idButton = this.id;
     // shuffled array - answer 1 is the correct answer but shown in alternate positions
@@ -87,52 +97,62 @@ $(".flag-choice").on("click", function () {
     $('#answer4').prop('disabled', true);
     $('#next-button').prop('disabled', false);
     $('#start-game').prop('disabled', true);
-    
+
     // check to see if we have played max questions 
     sumQuestions = parseInt(correctQuestions.innerText, 10) + parseInt(wrongQuestions.innerText, 10);
-       //game end modal scores 
+    //game end modal scores 
 
-       $("#c-score").text("Correct Answers  " + correctQuestions.innerText)
-       $("#w-score").text("Wrong answers  " + wrongQuestions.innerText)
-       $("#t-score").text("Total Answered  " + sumQuestions)
+    $("#t-score").text("Total Answered  " + sumQuestions)
+     
     if (maxQuestions == sumQuestions) {
+        $("#game-over-mess").show();
         console.log("exit = true")
-        return resetGame() 
-    }
+        $("#next-button").hide();
+         return setTimeout(resetGame, 2500); 
+     }
+    $("#c-score").text("Correct Answers  " + correctQuestions.innerText)
+    $("#w-score").text("Wrong answers  " + wrongQuestions.innerText)
+   
+                   
     resetState()
 });
 
 function resetState() {
-    // reset button states so after click of next, enable answer buttons 
+    // set buttons and active to play next question
+    // reset button states so after click of next, enable buttons for answer 
     $('#next-button').on("click", function () {
-    $('#answer1').prop('disabled', false);
-    $('#answer2').prop('disabled', false);
-    $('#answer3').prop('disabled', false);
-    $('#answer4').prop('disabled', false);
-    $('#answer1').addClass('btn-info').removeClass('btn-success');
+        $('#answer1').prop('disabled', false);
+        $('#answer2').prop('disabled', false);
+        $('#answer3').prop('disabled', false);
+        $('#answer4').prop('disabled', false);
+        $('#answer1').addClass('btn-info').removeClass('btn-success');
 
-    // disable next button
-    $('#next-button').prop('disabled', true);
-    $('#start-game').prop('disabled', false);
-    // call game start 
-    initNewgame()
+        // disable next button
+        $('#next-button').prop('disabled', true);
+        $('#start-game').prop('disabled', false);
+        // call game start 
+        initNewgame()
     });
 }
 
-function resetGame () {
-    $("#targ-flag").attr('src','assets/img/start-game.jpg');
+function resetGame() {
+    // hide next and reset button after end of game/reset button pressed 
+    $("#next-button").hide();
+    $("#reset-game").hide();
+    //load default values to page 
+    $("#targ-flag").attr('src', gameImg);
     $(".flag-choice").html("&nbsp")
     $('.flag-choice').prop('disabled', false);
     // enable start button - for new game  
     $('#start-game').prop('disabled', false);
     // remove green button after last answer is wrong 
     $('#answer1').addClass('btn-info').removeClass('btn-success');
+
     // show modal with game summary
     $("#GameEndModal").modal('show');
     // zero scores for next game
-    resetScore()
-    //enables game options after game end 
-    //$('#game-options').prop('disabled', false);
-}
     
-
+    resetScore()
+    // enables game options after game end 
+    $('#game-options').prop('disabled', false);
+}
